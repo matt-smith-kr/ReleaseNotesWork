187469383dbaab99de66013f25556dadc0a6fe7b
matt smith <matt.smith@kroger.com>
2022-02-07 15:01:03 -0800

    misc label

--- ReleaseNotesWork/main.swift
+++ ReleaseNotesWork/main.swift
@@ -7,16 +7,4 @@
 
 import Foundation
 
-print("Hello, World!")
-print("testing release notes")
-print("testing label")
-print("don't see in notes")
-print("trying again")
-print("don't see in notes...again")
-print("using Testing label")
-print("using label wontfix")
-print("using label duplicate")
-print("this is adding a description to my PR")
-print("use fake JIRA ID")
-print("Does this run again?")
-print("Maybe a merge conflict for duplicate label")
+print("clean up misc")
